84c82aea30db83c4a409ccfdaa7263319f28099f
Dawan Minhajul Islam <dminhaj.vrsty04@gmail.com>
2021-03-04 20:27:18 +0600

    Hello

---
+++ Association/Association/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Depertment den1 = new Depertment("Computer science", "CS");
+            Depertment den2 = new Depertment("Electrical Engineering", "EEE");
+            Student s1 = new Student ("Sadman Sakib", "33604", 3.25f);
+            Student s2 = new Student("Akil Ahmed", "42519", 3.01f);
+            s1.Depertment = den1;
+            s2.Depertment = den2;
+            s1.show();
+            s2.show();
+
+        }
+    }
+}
